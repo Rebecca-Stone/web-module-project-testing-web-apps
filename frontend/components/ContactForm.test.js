52564a73afdb3a2121451a2666508c1888f5a6fb
Rebecca-Stone <primstone16@gmail.com>
2022-03-10 18:41:40 -0600

    made the code more DRY

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -4,6 +4,13 @@ import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
 import ContactForm from "./ContactForm";
 
+const submitBtn = () => screen.getByText("Submit");
+const firstNameInput = () => screen.getByPlaceholderText("Edd");
+const lastNameInput = () => screen.getByPlaceholderText("Burke");
+const emailInput = () =>
+  screen.getByPlaceholderText("bluebill1049@hotmail.com");
+const messageInput = () => screen.getByLabelText("Message");
+
 beforeEach(() => {
   render(<ContactForm />);
 });
@@ -22,21 +29,23 @@ describe("ContactForm component", () => {
   });
 
   test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
-    const firstNameInput = screen.getByPlaceholderText("Edd");
-    fireEvent.change(firstNameInput, { target: { value: "a" } });
+    fireEvent.change(firstNameInput(), { target: { value: "a" } });
     const firstNameError = await screen.findByText(
       "Error: firstName must have at least 5 characters",
-      { exact: false }
+      {
+        exact: false,
+      }
     );
     expect(firstNameError).toBeInTheDocument();
   });
 
   test("renders THREE error messages if user enters no values into any fields.", async () => {
-    const submitBtn = screen.getByText("Submit");
-    fireEvent.click(submitBtn);
+    fireEvent.click(submitBtn());
     const firstNameError = await screen.findByText(
       "Error: firstName must have at least 5 characters",
-      { exact: false }
+      {
+        exact: false,
+      }
     );
     expect(firstNameError).toBeInTheDocument();
     const lastNameError = await screen.findByText(
@@ -52,12 +61,9 @@ describe("ContactForm component", () => {
   });
 
   test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
-    const firstNameInput = screen.getByPlaceholderText("Edd");
-    fireEvent.change(firstNameInput, { target: { value: "abcde" } });
-    const lastNameInput = screen.getByPlaceholderText("Burke");
-    fireEvent.change(lastNameInput, { target: { value: "abc" } });
-    const submitBtn = screen.getByText("Submit");
-    fireEvent.click(submitBtn);
+    fireEvent.change(firstNameInput(), { target: { value: "abcde" } });
+    fireEvent.change(lastNameInput(), { target: { value: "abc" } });
+    fireEvent.click(submitBtn());
     const emailError = await screen.findByText(
       "Error: email must be a valid email address",
       { exact: false }
@@ -66,8 +72,7 @@ describe("ContactForm component", () => {
   });
 
   test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    const emailInput = screen.getByPlaceholderText("bluebill1049@hotmail.com");
-    fireEvent.change(emailInput, { target: { value: "aaa" } });
+    fireEvent.change(emailInput(), { target: { value: "aaa" } });
     const emailError = await screen.findByText(
       "Error: email must be a valid email address",
       { exact: false }
@@ -76,81 +81,40 @@ describe("ContactForm component", () => {
   });
 
   test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    const firstNameInput = screen.getByPlaceholderText("Edd");
-    fireEvent.change(firstNameInput, { target: { value: "abcde" } });
-    const emailInput = screen.getByPlaceholderText("bluebill1049@hotmail.com");
-    fireEvent.change(emailInput, { target: { value: "aaa123@email.com" } });
-    const submitBtn = screen.getByText("Submit");
-    fireEvent.click(submitBtn);
+    fireEvent.change(firstNameInput(), { target: { value: "abcde" } });
+    fireEvent.change(emailInput(), { target: { value: "aaa123@email.com" } });
+    fireEvent.click(submitBtn());
     const lastNameError = await screen.findByText(
-      "lastName is a required field",
+      "Error: lastName is a required field",
       { exact: false }
     );
     expect(lastNameError).toBeInTheDocument();
   });
 
   test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {
-    const firstNameInput = screen.getByPlaceholderText("Edd");
-    fireEvent.change(firstNameInput, { target: { value: "abcde" } });
-    const lastNameInput = screen.getByPlaceholderText("Burke");
-    fireEvent.change(lastNameInput, { target: { value: "abc" } });
-    const emailInput = screen.getByPlaceholderText("bluebill1049@hotmail.com");
-    fireEvent.change(emailInput, { target: { value: "aaa123@email.com" } });
-
-    const submitBtn = screen.getByText("Submit");
-    fireEvent.click(submitBtn);
-
-    // const firstName = await screen.findByText("First Name: abcde", {
-    //   exact: false,
-    // });
-    // await screen.findByText(firstName);
-    // expect(firstName).toBeVisible();
-
-    // const lastName = await screen.findByText("Last Name: abc", {
-    //   exact: false,
-    // });
-    // await screen.findByText(lastName);
-    // expect(lastName).toBeVisible();
-    // const email = await screen.findByText("Email: aaa123@email.com", {
-    //   exact: false,
-    // });
-    // await screen.findByText(email);
-    // expect(email).toBeVisible();
+    fireEvent.change(firstNameInput(), { target: { value: "abcde" } });
+    fireEvent.change(lastNameInput(), { target: { value: "abc" } });
+    fireEvent.change(emailInput(), { target: { value: "aaa123@email.com" } });
+    fireEvent.click(submitBtn());
     const notThere = screen.queryByText("Message: abc", { exact: false });
     expect(notThere).not.toBeInTheDocument();
   });
 
   test("renders all fields text when all fields are submitted.", async () => {
-      const firstNameInput = screen.getByPlaceholderText("Edd");
-      fireEvent.change(firstNameInput, { target: { value: "abcde" } });
-      const lastNameInput = screen.getByPlaceholderText("Burke");
-      fireEvent.change(lastNameInput, { target: { value: "abc" } });
-      const emailInput = screen.getByPlaceholderText(
-        "bluebill1049@hotmail.com"
-      );
-      fireEvent.change(emailInput, { target: { value: "aaa123@email.com" } });
-
-      const messageInput = screen.getByLabelText("Message");
-      fireEvent.change(messageInput, { target: { value: "hello world"}});
-
-      const submitBtn = screen.getByText("Submit");
-      fireEvent.click(submitBtn);
-
-      const firstName = screen.getByTestId("firstnameDisplay");
-    //   await screen.findByText(firstName);
-      expect(firstName).toBeVisible();
-
-      const lastName = screen.getByTestId("lastnameDisplay");
-    //   await screen.findByText(lastName);
-      expect(lastName).toBeVisible();
-
+    fireEvent.change(firstNameInput(), { target: { value: "abcde" } });
+    fireEvent.change(lastNameInput(), { target: { value: "abc" } });
+    fireEvent.change(emailInput(), { target: { value: "aaa123@email.com" } });
+    fireEvent.change(messageInput(), { target: { value: "hello world" } });
+    fireEvent.click(submitBtn());
+    const firstName = screen.getByTestId("firstnameDisplay");
+    expect(firstName).toBeVisible();
+    const lastName = screen.getByTestId("lastnameDisplay");
+    expect(lastName).toBeVisible();
     const email = screen.getByTestId("emailDisplay", {
-        exact: false,
-      });
-    //   await screen.findByText(email);
-      expect(email).toBeVisible();
-
-      const message = screen.getByTestId("messageDisplay");
-      expect(message).toBeVisible();
+      exact: false,
+    });
+    expect(email).toBeVisible();
+    const message = screen.getByTestId("messageDisplay");
+    expect(message).toBeVisible();
   });
 });
